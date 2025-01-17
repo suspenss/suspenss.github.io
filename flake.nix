007629ba93678acc1b7c45b8da43ba594f61ac68
suspen <190865826+shusoyo@users.noreply.github.com>
2025-01-17 23:58:00 +0800

    Feature: add nix shell

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
 
     meme = {
       url = "github:reuixiy/hugo-theme-meme";
@@ -11,46 +10,43 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, meme }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, meme }: let
+    systems = [ "x86_64-linux" "x86_64-darwin" ];
 
-        blog = pkgs.stdenv.mkDerivation {
-          name = "blog";
-          # Exclude themes and public folder from build sources
-          src = ./.;
-          # Link theme to themes folder and build
-          buildPhase = ''
-            mkdir -p themes
-            ln -s ${meme} themes/meme
-            ls themes/meme/
-            ${pkgs.hugo}/bin/hugo --minify
-          '';
+    for-all-system = f: nixpkgs.lib.genAttrs
+      systems (system: f nixpkgs.legacyPackages.${system})
+    ;
+  in {
+    packages = for-all-system (pkgs: {
+      default = pkgs.stdenvNoCC.mkDerivation {
+        name = "blog";
+        src = ./.;
+        buildPhase = ''
+          mkdir -p themes
+          ln -s ${meme} themes/meme
+          ${pkgs.hugo}/bin/hugo --minify
+        '';
 
-          installPhase = ''
-            cp -r public $out
-          '';
+        installPhase = ''
+          cp -r public $out
+        '';
 
-          meta = with pkgs.lib; {
-            description = "ss's personal blog";
-            platforms = platforms.all;
-          };
+        meta = with pkgs.lib; {
+          description = "ss's personal blog";
+          platforms = platforms.all;
         };
-      in {
-        packages = {
-          blog = blog;
-          default = blog;
-        };
+      };
+    });
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ hugo ];
-          # Link theme to themes folder
-          shellHook = ''
-            echo Hello
-          '';
-        };
-      }
-    );
+    devShells = for-all-system (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [ hugo fish ];
+
+        shellHook = ''
+          echo hello hugo
+          exec fish
+        '';
+      };
+    });
+  };
 }
-
